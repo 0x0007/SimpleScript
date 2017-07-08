203c63559f5e2373807b7fb1ecb63d13edb31ec2
GoodLuckNetwork <761604382@qq.com>
2017-07-08 23:00:21 +0800

    simple blog, imooc note

---
+++ myblog/blog/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+	title=models.CharField(max_length=32,default='Title')
+	content=models.TextField(null=True)
+    #pub_time=models.DateTimeField(auto_now=True) # auto_now=True can't be modify in admin GUI
+	pub_time=models.DateTimeField(null=True)
+	def __unicode__(self):
+		return self.title
+	
